cd37bc4cf303540a3fcc71ea37da3f58a97f0930
Etan Joseph Heyman <etan@heyman.net>
2025-07-04 13:11:58 -0600

    Working images

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,21 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
         pathname: '/**',
       },
